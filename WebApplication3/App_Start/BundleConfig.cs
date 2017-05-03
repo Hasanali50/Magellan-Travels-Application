410409ab0561fc27200dbd138cbaf33e584f3038
Hasan Ali <hasanali50@live.com>
2017-05-02 23:49:46 -0700

    moi

--- WebApplication3/App_Start/BundleConfig.cs
+++ WebApplication3/App_Start/BundleConfig.cs
@@ -24,8 +24,10 @@ namespace WebApplication3
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                "~/Content/materialize/css/materialize.css",
+                "~/Content/bootstrap.css",
+                      "~/Content/site.css"
+                      ));
         }
     }
 }
