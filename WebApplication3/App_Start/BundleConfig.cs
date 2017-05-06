8f652ad4ed295e4aecbd15ea6964d92548a4b306
Hasan Ali <hasanali50@live.com>
2017-05-05 22:05:49 -0700

    lol

--- WebApplication3/App_Start/BundleConfig.cs
+++ WebApplication3/App_Start/BundleConfig.cs
@@ -24,8 +24,7 @@ namespace WebApplication3
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                "~/Content/materialize/css/materialize.css",
-                "~/Content/bootstrap.css",
+                               "~/Content/bootstrap.css",
                       "~/Content/site.css"
                       ));
         }
--- WebApplication3/Controllers/GalleryController.cs
+++ WebApplication3/Controllers/GalleryController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication3.Models;
+using WebApplication3.Models.ViewModels;
 
 namespace WebApplication3.Controllers
 {
@@ -11,8 +13,24 @@ namespace WebApplication3.Controllers
         // GET: Gallery
         public ActionResult Index()
         {
-            var Pictures = Globals.GetDatabase().Gallery.ToList();
-            return View(Pictures);
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult PhotoSave(GalleryViewModel model, HttpPostedFileBase file)
+        {
+            var db = Globals.GetDatabase();
+            var gallery = new Gallery();
+            
+            var fileName = file.FileName;
+            string path = System.IO.Path.Combine(Server.MapPath("~/Img"), fileName);
+            file.SaveAs(path);
+            string dbPath = System.IO.Path.Combine("~/Img", fileName);
+            gallery.PictureDesc = model.PictureDesc;
+            gallery.PicturePath = dbPath;
+            db.Gallery.Add(gallery);
+            db.SaveChanges();
+            return RedirectToAction(nameof(Index));
         }
     }
 }
